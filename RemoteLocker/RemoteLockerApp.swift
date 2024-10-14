ca13adff13ded3d1361d56b0dcfae0ef638d8dfc
tim <nagisa12164@163.com>
2024-10-14 21:19:06 +0800

    Initial Commit

---
+++ RemoteLocker/RemoteLockerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemoteLockerApp.swift
+//  RemoteLocker
+//
+//  Created by tim on 2024/10/14.
+//
+
+import SwiftUI
+
+@main
+struct RemoteLockerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
